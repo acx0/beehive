df6562247a7e7cf384ff37fe657136b315ef3d2c
Sam Lidder <sam.lidder@gmail.com>
2016-04-23 18:51:17 -0700

    add virtual dtor for log_policy_interface

--- logger.h
+++ logger.h
@@ -24,6 +24,10 @@ enum severity_type
 class log_policy_interface
 {
 public:
+    virtual ~log_policy_interface()
+    {
+    }
+
     virtual void open_ostream(const std::string &name) = 0;
     virtual void close_ostream() = 0;
     virtual void write(const std::string &str) = 0;
