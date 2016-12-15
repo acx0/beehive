5a35db9927f0247d7214b34e63540d1301390908
Sam Lidder <sam.lidder@gmail.com>
2016-12-14 19:00:33 -0800

    fix at_command value tests to use ASSERT_STREQ

--- at_command_test.cc
+++ at_command_test.cc
@@ -4,25 +4,25 @@
 
 TEST(ATCommandTest, ConstValuesSpec)
 {
-    ASSERT_EQ(at_command::RESPONSE_SUCCESS, "OK");
-    ASSERT_EQ(at_command::RESPONSE_ERROR, "ERROR");
-    ASSERT_EQ(at_command::AT_PREFIX, "AT");
-    ASSERT_EQ(at_command::API_ENABLE, "AP");
-    ASSERT_EQ(at_command::EXIT_COMMAND_MODE, "CN");
-    ASSERT_EQ(at_command::SOURCE_ADDRESS_16_BIT, "MY");
-    ASSERT_EQ(at_command::SERIAL_NUMBER_HIGH, "SH");
-    ASSERT_EQ(at_command::SERIAL_NUMBER_LOW, "SL");
-    ASSERT_EQ(at_command::SOFTWARE_RESET, "FR");
-    ASSERT_EQ(at_command::MAC_MODE, "MM");
-    ASSERT_EQ(at_command::INTERFACE_DATA_RATE, "BD");
-    ASSERT_EQ(at_command::PERSONAL_AREA_NETWORK_ID, "ID");
-    ASSERT_EQ(at_command::CHANNEL, "CH");
-    ASSERT_EQ(at_command::XBEE_RETRIES, "RR");
-    ASSERT_EQ(at_command::POWER_LEVEL, "PL");
-    ASSERT_EQ(at_command::CCA_THRESHOLD, "CA");
-    ASSERT_EQ(at_command::RESTORE_DEFAULTS, "RE");
-    ASSERT_EQ(at_command::WRITE, "WR");
-    ASSERT_EQ(at_command::REGISTER_QUERY, "");
+    ASSERT_STREQ(at_command::RESPONSE_SUCCESS, "OK");
+    ASSERT_STREQ(at_command::RESPONSE_ERROR, "ERROR");
+    ASSERT_STREQ(at_command::AT_PREFIX, "AT");
+    ASSERT_STREQ(at_command::API_ENABLE, "AP");
+    ASSERT_STREQ(at_command::EXIT_COMMAND_MODE, "CN");
+    ASSERT_STREQ(at_command::SOURCE_ADDRESS_16_BIT, "MY");
+    ASSERT_STREQ(at_command::SERIAL_NUMBER_HIGH, "SH");
+    ASSERT_STREQ(at_command::SERIAL_NUMBER_LOW, "SL");
+    ASSERT_STREQ(at_command::SOFTWARE_RESET, "FR");
+    ASSERT_STREQ(at_command::MAC_MODE, "MM");
+    ASSERT_STREQ(at_command::INTERFACE_DATA_RATE, "BD");
+    ASSERT_STREQ(at_command::PERSONAL_AREA_NETWORK_ID, "ID");
+    ASSERT_STREQ(at_command::CHANNEL, "CH");
+    ASSERT_STREQ(at_command::XBEE_RETRIES, "RR");
+    ASSERT_STREQ(at_command::POWER_LEVEL, "PL");
+    ASSERT_STREQ(at_command::CCA_THRESHOLD, "CA");
+    ASSERT_STREQ(at_command::RESTORE_DEFAULTS, "RE");
+    ASSERT_STREQ(at_command::WRITE, "WR");
+    ASSERT_STREQ(at_command::REGISTER_QUERY, "");
     ASSERT_EQ(at_command::CR, '\r');
 }
 
